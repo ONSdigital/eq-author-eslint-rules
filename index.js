3a3617445f208db031fce14a0e08e0cefee06074
Benn Pearson <bennpearson@gmail.com>
2017-07-06 16:26:41 +0100

    added eslint rules

---
+++ index.js
@@ -0,0 +1,27 @@
+
+module.exports = {
+  "extends": ["./index", "react-app", "plugin:react/recommended", ],
+  "plugins": [
+    "react",
+    "babel",
+    "jasmine"
+  ],
+  "env": {
+    "jasmine": true,
+    "browser": true
+  },
+  "rules": {
+    "react/display-name": "off",
+    "react/prop-types": "warn",
+    "react/forbid-prop-types": "warn",
+    "react/no-danger": "error",
+    "react/no-did-mount-set-state": "error",
+    "react/no-did-update-set-state": "error",
+    "react/self-closing-comp": "error",
+
+    "react/jsx-boolean-value": "error",
+    "react/jsx-handler-names": "error",
+    "react/jsx-no-bind": "warn",
+    "react/jsx-pascal-case": "error",
+
+};
